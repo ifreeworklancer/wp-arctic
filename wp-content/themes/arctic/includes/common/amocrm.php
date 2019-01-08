e96a1a7df9e0158e96e18f60c5a74745500bffe4
Oleh Talanoff <talanov.o@gmail.com>
2019-01-08 16:27:52 +0200

    AmoCRM

--- wp-content/themes/arctic/includes/common/amocrm.php
+++ wp-content/themes/arctic/includes/common/amocrm.php
@@ -5,9 +5,13 @@ use AmoCRM\Handler;
 use AmoCRM\Lead;
 use AmoCRM\Request;
 
-require_once (__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../vendor/autoload.php');
 
 try {
+	$user = [
+		'name' => $data['user-name'],
+		'phone' => $data['phone']
+	];
 	$api = new Handler('arktikazp', 'dimplex.nic@gmail.com');
 
 	$lead = new Lead();
@@ -18,7 +22,7 @@ try {
 		->setCustomField($api->config['BottlesFieldId'], $data['value'])
 		->setCustomField($api->config['CommentFieldId'], $data['message'])
 		->setCustomField($api->config['AddressFieldId'], $data['place'])
-		->setTags([$data['user-name'], $data['phone']]);
+		->setTags([$user['name'], $user['phone']]);
 
 	$api->request(new Request(Request::SET, $lead));
 
@@ -26,21 +30,23 @@ try {
 
 	$contact = new Contact();
 	$contact
-		->setName($data['user-name'])
+		->setName($user['name'])
 		->setResponsibleUserId($api->config['ResponsibleUserId'])
 		->setLinkedLeadsId($lead)
-		->setCustomField($api->config['ContactFieldPhone'], phone_filter($data['phone']), 'MOB');
+		->setCustomField($api->config['ContactFieldPhone'], $user['phone'], 'MOB');
 
-//	$api->request(new Request(Request::GET, ['query' => phone_filter($data['phone'])], ['contacts', 'list']));
-//
-//	$contact_exists = ($api->result) ? $api->result->contacts[0] : false;
-//
-//	if ($contact_exists) {
-//		$contact
-//			->setUpdate($contact_exists->id, $contact_exists->last_modified + 1)
-//			->setResponsibleUserId($contact_exists->responsible_user_id)
-//			->setLinkedLeadsId($contact_exists->linked_leads_id);
-//	}
+	$api->request(new Request(Request::GET, ['query' => $user['phone']], ['contacts', 'list']));
+
+	$contact_exists = ($api->result) ? $api->result->contacts[0] : false;
+
+	if ($contact_exists) {
+		$contact
+			->setUpdate($contact_exists->id, $contact_exists->last_modified + 1)
+			->setResponsibleUserId($contact_exists->responsible_user_id)
+			->setLinkedLeadsId($contact_exists->linked_leads_id);
+	}
+
+	$api->request(new Request(Request::SET, $contact));
 } catch (\Exception $e) {
 }
 
