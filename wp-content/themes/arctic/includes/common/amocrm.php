670fb31eda742d6e1627ea2b4081a7c58d4406dc
Oleh Talanoff <talanov.o@gmail.com>
2019-01-08 16:10:40 +0200

    AmoCRM

--- wp-content/themes/arctic/includes/common/amocrm.php
+++ wp-content/themes/arctic/includes/common/amocrm.php
@@ -31,6 +31,8 @@ try {
 		->setLinkedLeadsId($lead)
 		->setCustomField($api->config['ContactFieldPhone'], phone_filter($data['phone']), 'MOB');
 
+	var_dump($contact); die();
+
 	$api->request(new Request(Request::GET, ['query' => phone_filter($data['phone'])], ['contacts', 'list']));
 
 	$contact_exists = ($api->result) ? $api->result->contacts[0] : false;
