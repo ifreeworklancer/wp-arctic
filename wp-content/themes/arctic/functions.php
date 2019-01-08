f9ee4d787b62dd4c93d608203f9413e27fe7db82
Oleh Talanoff <talanov.o@gmail.com>
2019-01-08 15:23:27 +0200

    AmoCRM

--- wp-content/themes/arctic/functions.php
+++ wp-content/themes/arctic/functions.php
@@ -11,6 +11,7 @@ function theme_setup()
 		]
 	);
 }
+
 add_action('after_setup_theme', 'theme_setup');
 // Cleanup header
 remove_action('wp_head', 'print_emoji_detection_script', 7);
@@ -25,17 +26,19 @@ show_admin_bar(false);
 function theme_scripts()
 {
 	wp_deregister_script('wp-embed');
-//	wp_deregister_script('jquery');
-//    wp_deregister_script('jquery-migrate');
-    
+	//	wp_deregister_script('jquery');
+	//    wp_deregister_script('jquery-migrate');
+
 	wp_enqueue_script('app', get_theme_file_uri('dist/app.js'), null, '', true);
 }
+
 add_action('wp_enqueue_scripts', 'theme_scripts');
 // Enqueue styles
 function theme_styles()
 {
 	wp_enqueue_style('theme-app', get_theme_file_uri('dist/app.css'), null, null);
 }
+
 add_action('wp_enqueue_scripts', 'theme_styles');
 
 // Debug
@@ -48,7 +51,8 @@ function dd($value)
 }
 
 if (!function_exists('phone_filter')) {
-	function phone_filter($phone) {
+	function phone_filter($phone)
+	{
 		return str_replace([' ', '(', ')', '-'], '', trim($phone));
 	}
 }
@@ -66,4 +70,17 @@ function theme_customize_register($wp_customize)
 		'type' => 'textarea',
 	]);
 }
+
 add_action('customize_register', 'theme_customize_register');
+
+/*
+ * Get CF7 data for amoCRM and sync
+ */
+add_action('wpcf7_before_send_mail', 'amoCrmSync');
+function amoCrmSync($contact_form)
+{
+	if ($submission = WPCF7_Submission::get_instance()) {
+		$data = $submission->get_posted_data();
+		require_once 'includes/common/amocrm.php';
+	}
+}
---
+++ wp-content/themes/arctic/includes/common/amocrm.php
@@ -0,0 +1,49 @@
+<?php
+
+use AmoCRM\Contact;
+use AmoCRM\Handler;
+use AmoCRM\Lead;
+use AmoCRM\Request;
+
+require_once '../../vendor/autoload.php';
+
+try {
+	$api = new Handler('domain', 'user@example.com');
+
+	$lead = new Lead();
+	$lead
+		->setName('Заявка с сайта')
+		->setResponsibleUserId($api->config['ResponsibleUserId'])
+		->setStatusId($api->config['LeadStatusId'])
+		->setCustomField($api->config['BottlesFieldId'], $data['value'])
+		->setCustomField($api->config['CommentFieldId'], $data['message'])
+		->setCustomField($api->config['AddressFieldId'], $data['message'])
+		->setTags([$data['user-name'], $data['phone']]);
+
+	$api->request(new Request(Request::SET, $lead));
+
+	$contact = new Contact();
+	$contact
+		/* Имя */
+		->setName($data['user-name'])
+		->setResponsibleUserId($api->config['ResponsibleUserId'])
+		->setLinkedLeadsId($api->last_insert_id)
+		->setCustomField(
+			$api->config['ContactFieldPhone'],
+			$data['phone'],
+			'MOB'
+		);
+
+	$api->request(new Request(Request::GET, ['query' => $data['phone']], ['contacts', 'list']));
+
+	$contact_exists = ($api->result) ? $api->result->contacts[0] : false;
+
+	if ($contact_exists) {
+		$contact
+			->setUpdate($contact_exists->id, $contact_exists->last_modified + 1)
+			->setResponsibleUserId($contact_exists->responsible_user_id)
+			->setLinkedLeadsId($contact_exists->linked_leads_id);
+	}
+} catch (\Exception $e) {
+}
+
