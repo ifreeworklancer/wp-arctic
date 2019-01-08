b157f82b3ab25a72166c0164f7e45664f934a641
Oleh Talanoff <talanov.o@gmail.com>
2019-01-08 16:13:10 +0200

    AmoCRM

--- wp-content/themes/arctic/includes/common/amocrm.php
+++ wp-content/themes/arctic/includes/common/amocrm.php
@@ -31,16 +31,16 @@ try {
 		->setLinkedLeadsId($lead)
 		->setCustomField($api->config['ContactFieldPhone'], phone_filter($data['phone']), 'MOB');
 
-	$api->request(new Request(Request::GET, ['query' => phone_filter($data['phone'])], ['contacts', 'list']));
-
-	$contact_exists = ($api->result) ? $api->result->contacts[0] : false;
-
-	if ($contact_exists) {
-		$contact
-			->setUpdate($contact_exists->id, $contact_exists->last_modified + 1)
-			->setResponsibleUserId($contact_exists->responsible_user_id)
-			->setLinkedLeadsId($contact_exists->linked_leads_id);
-	}
+//	$api->request(new Request(Request::GET, ['query' => phone_filter($data['phone'])], ['contacts', 'list']));
+//
+//	$contact_exists = ($api->result) ? $api->result->contacts[0] : false;
+//
+//	if ($contact_exists) {
+//		$contact
+//			->setUpdate($contact_exists->id, $contact_exists->last_modified + 1)
+//			->setResponsibleUserId($contact_exists->responsible_user_id)
+//			->setLinkedLeadsId($contact_exists->linked_leads_id);
+//	}
 } catch (\Exception $e) {
 }
 
