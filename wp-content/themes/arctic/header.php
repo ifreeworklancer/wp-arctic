1100ade27dfd45ef6cf84f99f3ecf5d44df70f00
oleg.t <talanov.o@gmail.com>
2020-11-17 16:48:38 +0200

    Analytics updated

--- wp-content/themes/arctic/header.php
+++ wp-content/themes/arctic/header.php
@@ -5,86 +5,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <?php wp_head(); ?>
 
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-85554531-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
-
-    <script>
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '506799066368931');
-      fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=506799066368931&ev=PageView&noscript=1"
-        /></noscript>
+    <?php require_once(__DIR__ . '/includes/common/ga.php') ?>
+    <?php require_once(__DIR__ . '/includes/common/fb.php') ?>
 </head>
 
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
+<?php require_once (__DIR__. '/includes/common/fb_noscript.php'); ?>
 
-    <header id="app-header">
-        <div class="container position-relative">
-            <div class="row justify-content-between align-items-center">
-                <div class="col-sm-3 col-lg-2">
-                    <a href="<?php echo site_url(); ?>" class="logo">
-                        <img src="<?php echo get_theme_file_uri('images/icon/logo/logo.png') ?>" alt="logo">
-                    </a>
-                </div>
-                <div class="col-sm-9 col-xl-7 d-flex justify-content-between align-items-center">
-                    <nav class="menu w-100 mr-lg-5" id="nav-menu">
-                        <div class="menu-content">
-                            <ul class="menu-list d-flex flex-column flex-lg-row justify-content-between align-items-center list-unstyled">
-                                <li class="menu-list-item">
-                                    <a href="#services" class="scroll-link">
-                                        сервисы
-                                    </a>
-                                </li>
-                                <li class="menu-list-item">
-                                    <a href="#about" class="scroll-link">
-                                        о нас
-                                    </a>
-                                </li>
-                                <li class="menu-list-item">
-                                    <a href="#stock" class="scroll-link">
-                                        акции
-                                    </a>
-                                </li>
-                                <li class="menu-list-item">
-                                    <a href="#contacts" class="scroll-link">
-                                        контакты
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <a href="#" class="btn btn-outline-light d-none d-lg-inline-flex open-order">
-                        Заказать воду
-                    </a>
-                </div>
+<header id="app-header">
+    <div class="container position-relative">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-sm-3 col-lg-2">
+                <a href="<?php echo site_url(); ?>" class="logo">
+                    <img src="<?php echo get_theme_file_uri('images/icon/logo/logo.png') ?>" alt="logo">
+                </a>
             </div>
-            <div class="burger-menu d-flex d-lg-none flex-column justify-content-center align-items-center">
-                <div class="line line--top"></div>
-                <div class="line line--middle"></div>
-                <div class="line line--bottom"></div>
+            <div class="col-sm-9 col-xl-7 d-flex justify-content-between align-items-center">
+                <nav class="menu w-100 mr-lg-5" id="nav-menu">
+                    <div class="menu-content">
+                        <ul class="menu-list d-flex flex-column flex-lg-row justify-content-between align-items-center list-unstyled">
+                            <li class="menu-list-item">
+                                <a href="#services" class="scroll-link">
+                                    сервисы
+                                </a>
+                            </li>
+                            <li class="menu-list-item">
+                                <a href="#about" class="scroll-link">
+                                    о нас
+                                </a>
+                            </li>
+                            <li class="menu-list-item">
+                                <a href="#stock" class="scroll-link">
+                                    акции
+                                </a>
+                            </li>
+                            <li class="menu-list-item">
+                                <a href="#contacts" class="scroll-link">
+                                    контакты
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <a href="#" class="btn btn-outline-light d-none d-lg-inline-flex open-order">
+                    Заказать воду
+                </a>
             </div>
         </div>
-    </header>
+        <div class="burger-menu d-flex d-lg-none flex-column justify-content-center align-items-center">
+            <div class="line line--top"></div>
+            <div class="line line--middle"></div>
+            <div class="line line--bottom"></div>
+        </div>
+    </div>
+</header>
 
-    <main>
+<main>
 
---
+++ wp-content/themes/arctic/includes/common/fb_noscript.php
@@ -0,0 +1,1 @@
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=506799066368931&ev=PageView&noscript=1"/></noscript>
